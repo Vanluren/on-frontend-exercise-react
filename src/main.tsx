15f126db584d6705bed3f2b6ed8791cbc9a75364
Villads Nielsen <villads.nielsen@on-running.com>
2023-09-26 16:27:48 +0200

    refactor(context): Update import path for QuizProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import router from '@/router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { QuizProvider } from './context';
+import { QuizProvider } from './contexts/QuizContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
